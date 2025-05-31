ff0e1bdd9ee60c5c9c247ad005a5416042d83fbf
ahmedmahddi <155826291+ahmedmahddi@users.noreply.github.com>
2025-05-31 21:39:00 +0100

    05: search filters

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -1,17 +1,41 @@
-
+import configPromise from '@payload-config'
+import { getPayload } from 'payload';
+import { Category } from '@/payload-types';
 
 import { Footer } from "./footer";
 import { Navbar } from "./navbar";
+import { SearchFilters } from "./search-filters";
+
 
 interface Props {
     children: React.ReactNode;
 }
 
 
-const layout = ({ children }: Props) => {
+const layout = async ({ children }: Props) => {
+    const payload = await getPayload({
+        config: configPromise,
+    })
+
+    const data = await payload.find({
+        collection: 'categories',
+        depth: 1,
+        pagination: false,
+        where: { parent: { exists: false } },
+    })
+
+    const formatedData = data.docs.map((doc) => ({
+        ...doc,
+        subcategories: (doc.subcategories?.docs ?? []).map((doc) => ({
+            ...(doc as Category)
+        })),
+    }))
+
+    console.log({ data, formatedData })
     return (
         <div className="flex flex-col min-h-screen">
             <Navbar />
+            <SearchFilters data={formatedData} />
             <div className="flex-1 bg-[#F4F4F0]">
                 {children}
             </div>
---
+++ src/app/(app)/(home)/search-filters/categories.tsx
@@ -0,0 +1,21 @@
+import { Category } from "@/payload-types";
+import { CategoryDropdown } from "./category-dropdown";
+
+interface Props {
+    data: any; // Adjust the type as needed
+}
+
+export const Categories = ({ data }: Props) => {
+    return (
+        <div className="relative w-full">
+            <div className="flex flex-wrap items-center">
+                {data.map((category: Category) => (
+                    <div key={category.id}>
+                        <CategoryDropdown category={category} isActive={false} isNavigationHovered={false} />
+                    </div>
+
+                ))}
+            </div>
+        </div>
+    )
+}
