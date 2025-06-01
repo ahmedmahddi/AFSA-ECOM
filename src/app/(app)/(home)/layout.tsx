2a7c73ff98fc58542dc316a40db39aa61b0c3544
ahmedmahddi <155826291+ahmedmahddi@users.noreply.github.com>
2025-06-01 14:11:44 +0100

    06:categories finalization

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -5,6 +5,7 @@ import { Category } from '@/payload-types';
 import { Footer } from "./footer";
 import { Navbar } from "./navbar";
 import { SearchFilters } from "./search-filters";
+import { CustomCategory } from './types';
 
 
 interface Props {
@@ -22,16 +23,17 @@ const layout = async ({ children }: Props) => {
         depth: 1,
         pagination: false,
         where: { parent: { exists: false } },
+        sort: 'name',
     })
 
-    const formatedData = data.docs.map((doc) => ({
+    const formatedData: CustomCategory[] = data.docs.map((doc) => ({
         ...doc,
         subcategories: (doc.subcategories?.docs ?? []).map((doc) => ({
-            ...(doc as Category)
+            ...(doc as Category),
+            subcategories: undefined
         })),
     }))
 
-    console.log({ data, formatedData })
     return (
         <div className="flex flex-col min-h-screen">
             <Navbar />
