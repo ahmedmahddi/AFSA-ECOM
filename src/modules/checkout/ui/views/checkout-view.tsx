b91cb286b2494a0191dcaa7c221ce9eb3f0e4ca2
ahmedmahddi <155826291+ahmedmahddi@users.noreply.github.com>
2025-08-20 00:07:48 +0100

    23:Aggregating reviews

--- src/modules/checkout/ui/views/checkout-view.tsx
+++ src/modules/checkout/ui/views/checkout-view.tsx
@@ -52,7 +52,6 @@ export const CheckoutView = ({ tenantSlug }: CheckoutViewProps) => {
 
   useEffect(() => {
     if (states.success) {
-
       setStates({
         success: false,
         cancel: false
