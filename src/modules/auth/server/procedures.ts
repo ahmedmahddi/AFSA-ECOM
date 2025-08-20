70af3baeab49e857b06b02f16cb2bec33ad8e25e
ahmedmahddi <155826291+ahmedmahddi@users.noreply.github.com>
2025-08-20 13:44:30 +0100

    24:added things from 25

--- src/modules/auth/server/procedures.ts
+++ src/modules/auth/server/procedures.ts
@@ -5,6 +5,7 @@ import { headers as getHeaders } from "next/headers";
 
 import { loginSchema, registerSchema } from "../schemas";
 import { generateAuthCookie } from "../utils";
+import { stripe } from "@/lib/stripe";
 export const authRouter = createTRPCRouter({
   session: baseProcedure.query(async ({ ctx }) => {
     const headers = await getHeaders();
@@ -33,12 +34,19 @@ export const authRouter = createTRPCRouter({
         });
       }
 
+      const account = await stripe.accounts.create({})
+      if (!account) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "Failed to create account",
+        });
+      }
       const tenant = await ctx.db.create({
-        collection:"tenants",
-        data:{
-          name:input.username,
-          slug:input.username,
-          stripeAccountId:"test"
+        collection: "tenants",
+        data: {
+          name: input.username,
+          slug: input.username,
+          stripeAccountId: account.id
         }
       })
 
@@ -48,7 +56,7 @@ export const authRouter = createTRPCRouter({
           email: input.email,
           username: input.username,
           password: input.password,
-          tenants:[
+          tenants: [
             {
               tenant: tenant.id
             }
