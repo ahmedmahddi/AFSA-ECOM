4aaebd5a79aabac285aae58695cd7baab9d1f56a
ahmedmahddi <155826291+ahmedmahddi@users.noreply.github.com>
2025-08-21 14:54:59 +0100

    28: deployment

--- src/app/(app)/(auth)/sign-up/page.tsx
+++ src/app/(app)/(auth)/sign-up/page.tsx
@@ -3,6 +3,7 @@ import { redirect } from "next/navigation";
 
 import { SignUpView } from "@/modules/auth/ui/views/sign-up-view"
 
+export const dynamic = "force-dynamic"
 const page = async () => {
     const session = await caller.auth.session();
     if (session.user) {
--- src/app/(app)/(tenants)/tenants/[slug]/(home)/page.tsx
+++ src/app/(app)/(tenants)/tenants/[slug]/(home)/page.tsx
@@ -11,6 +11,7 @@ interface Props {
   searchParams: Promise<SearchParams>;
   params: Promise<{ slug: string }>;
 }
+export const dynamic = "force-dynamic"
 
 const pages = async ({ params, searchParams }: Props) => {
   const { slug } = await params;
